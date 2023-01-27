07deb7abd5a73a71221d1a9da744d313036d2e83
Juan Martin Saravia Da Silva <saraviamartin16@hotmail.com>
2023-01-27 15:27:39 -0300

    second commit to do it again later

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-/*
-We store our game status element here to allow us to more easily 
-use it later on 
-*/
+
 const statusDisplay = document.querySelector('.game--status');
 let gameActive = true;
 let currentPlayer = "X";
@@ -9,15 +6,10 @@ let gameState = ["", "", "", "", "", "", "", "", ""];
 const winningMessage = () => `Player ${currentPlayer} has won!`;
 const drawMessage = () => `Game ended in a draw!`;
 const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
-/*
-We set the inital message to let the players know whose turn it is
-*/
+
 statusDisplay.innerHTML = currentPlayerTurn();
 function handleCellPlayed(clickedCell, clickedCellIndex) {
-    /*
-    We update our internal game state to reflect the played move, 
-    as well as update the user interface to reflect the played move
-    */
+
         gameState[clickedCellIndex] = currentPlayer;
         clickedCell.innerHTML = currentPlayer;
     }
@@ -56,46 +48,29 @@ function handleCellPlayed(clickedCell, clickedCellIndex) {
             gameActive = false;
             return;
         }
-    /* 
-    We will check weather there are any values in our game state array 
-    that are still not populated with a player sign
-    */
+
         let roundDraw = !gameState.includes("");
         if (roundDraw) {
             statusDisplay.innerHTML = drawMessage();
             gameActive = false;
             return;
         }
-    /*
-    If we get to here we know that the no one won the game yet, 
-    and that there are still moves to be played, so we continue by changing the current player.
-    */
+    
         handlePlayerChange();
     }
 
 function handleCellClick(clickedCellEvent) {
-        /*
-        We will save the clicked html element in a variable for easier further use
-        */    
+        
             const clickedCell = clickedCellEvent.target;
-        /*
-        Here we will grab the 'data-cell-index' attribute from the clicked cell to identify where that cell is in our grid. 
-        Please note that the getAttribute will return a string value. Since we need an actual number we will parse it to an 
-        integer(number)
-        */
+        
             const clickedCellIndex = parseInt(
             clickedCell.getAttribute('data-cell-index')
             );
-        /* 
-        Next up we need to check whether the call has already been played, 
-        or if the game is paused. If either of those is true we will simply ignore the click.
-        */
+        
             if (gameState[clickedCellIndex] !== "" || !gameActive) {
                 return;
             }
-        /* 
-        If everything if in order we will proceed with the game flow
-        */    
+            
             handleCellPlayed(clickedCell, clickedCellIndex);
             handleResultValidation();
         }
