47ba790844a44335de9962cae292913ae512a94a
Juan Martin Saravia Da Silva <saraviamartin16@hotmail.com>
2023-02-02 20:28:12 -0300

    añadiendo javascript al juego

--- script-START.js
+++ script-START.js
@@ -7,5 +7,30 @@ const victoryMensaje = () => `El jugador ${currentPlayer} ha ganado!`;
 const drawMensaje = () => `Ha terminado en un empate!`;
 const currentPlayerTurn = () => `Es el turno de ${currentPlayer}`;
 
+/*Vamos a dar un mensaje inicial para ver quien arranca el juego  */
 
+statusDisplay.innerHTML = currentPlayer();
+function handleCellPlayed(clickedCell, clickedCellIndex){
+    gameStatus[clickedCellIndex] = currentPlayer;
+    clickedCell.innerHTML = currentPlayer;
+}
+function handlePlayerChange(){
+
+}
+
+
+/*En handleCellClick, tenemos que manejar dos cosas.
+ Primero tenemos que comprobar si ya se ha hecho clic en la celda. Si no continúa nuestro juego. */
+function handleCellClick(clickedCellEvent){
+    const clickedCell = clickedCellEvent.target;
+    const clickedCellIndex = parseInt(
+        clickedCellEvent.getAttibute(data-cell-index)
+    );
+    handleCellPlayed(clickedCell, clickedCellIndex);
+}
+
+
+function handleRestartGame(){
+
+}
 
