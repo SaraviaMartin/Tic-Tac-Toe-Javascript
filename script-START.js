0a97c71722fbb0c1455c8b971c3fb90211d12695
Juan Martin Saravia Da Silva <saraviamartin16@hotmail.com>
2023-02-02 20:41:41 -0300

    añadiendo la funcion que valida cada turno

--- script-START.js
+++ script-START.js
@@ -14,21 +14,37 @@ function handleCellPlayed(clickedCell, clickedCellIndex){
     gameStatus[clickedCellIndex] = currentPlayer;
     clickedCell.innerHTML = currentPlayer;
 }
-function handlePlayerChange(){
-
-}
-
 
 /*En handleCellClick, tenemos que manejar dos cosas.
  Primero tenemos que comprobar si ya se ha hecho clic en la celda. Si no continúa nuestro juego. */
 function handleCellClick(clickedCellEvent){
     const clickedCell = clickedCellEvent.target;
     const clickedCellIndex = parseInt(
-        clickedCellEvent.getAttibute(data-cell-index)
+        clickedCellEvent.getAttibute(data-cell-index)/*GetAttribute devolvera un string, como nosotros necesitamos un numero real lo trataremos como un entero*/ 
     );
     handleCellPlayed(clickedCell, clickedCellIndex);
 }
+function handlePlayerChange(){
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+    statusDisplay.currentPlayer = currentPlayerTurn();
+}
 
+const winningConditions = [
+    [0, 1, 2]
+    [3, 4, 5]
+    [6, 7, 8]
+    [0, 3, 6]
+    [1, 4, 7]
+    [2, 5, 8]
+    [0, 4, 8]
+    [2, 4, 6]
+]
+function handleResultValidation(){
+    let roundWon = false;
+    for(let i = 0; i <=7; i++){
+        
+    }
+}
 
 function handleRestartGame(){
 
